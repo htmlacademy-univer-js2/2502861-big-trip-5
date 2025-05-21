1d4c98c58770b48f85eaa0eadb11c46235a54400
Легков Никита <nik.legkov@mail.ru>
2025-05-21 09:43:21 +0500

    Большие перемены (часть 2)

--- src/framework/render.js
+++ src/framework/render.js
@@ -1,80 +1,85 @@
 import AbstractView from './view/abstract-view.js';
 
- /** @enum {string} Перечисление возможных позиций для отрисовки */
- const RenderPosition = {
-   BEFOREBEGIN: 'beforebegin',
-   AFTERBEGIN: 'afterbegin',
-   BEFOREEND: 'beforeend',
-   AFTEREND: 'afterend',
- };
+/** @enum {string} Перечисление возможных позиций для отрисовки */
+const RenderPosition = {
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};
 
- /**
-  * Функция для создания элемента на основе разметки
-  * @param {string} template Разметка в виде строки
-  * @returns {HTMLElement} Созданный элемент
-  */
- function createElement(template) {
-   const newElement = document.createElement('div');
-   newElement.innerHTML = template;
+/**
+ * Функция для создания элемента на основе разметки
+ * @param {string} template Разметка в виде строки
+ * @returns {HTMLElement} Созданный элемент
+ */
+function createElement(template) {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
 
-   return newElement.firstElementChild;
- }
+  return newElement.firstElementChild;
+}
 
- /**
-  * Функция для отрисовки элемента
-  * @param {AbstractView} component Компонент, который должен был отрисован
-  * @param {HTMLElement} container Элемент в котором будет отрисован компонент
-  * @param {string} place Позиция компонента относительно контейнера. По умолчанию - `beforeend`
-  */
- function render(component, container, place = RenderPosition.BEFOREEND) {
-   if (!(component instanceof AbstractView)) {
-     throw new Error('Can render only components');
-   }
+/**
+ * Функция для отрисовки элемента
+ * @param {AbstractView} component Компонент, который должен был отрисован
+ * @param {HTMLElement} container Элемент в котором будет отрисован компонент
+ * @param {string} place Позиция компонента относительно контейнера. По умолчанию - `beforeend`
+ */
+function render(component, container, place = RenderPosition.BEFOREEND) {
+  if (!(component instanceof AbstractView)) {
+    throw new Error('Can render only components');
+  }
 
-   if (container === null) {
-     throw new Error('Container element doesn\'t exist');
-   }
+  if (container === null) {
+    throw new Error('Container element doesn\'t exist');
+  }
 
-   container.insertAdjacentElement(place, component.element);
- }
+  container.insertAdjacentElement(place, component.element);
+}
 
- /**
-  * Функция для замены одного компонента на другой
-  * @param {AbstractView} newComponent Компонент, который нужно показать
-  * @param {AbstractView} oldComponent Компонент, который нужно скрыть
-  */
- function replace(newComponent, oldComponent) {
-   if (!(newComponent instanceof AbstractView && oldComponent instanceof AbstractView)) {
-     throw new Error('Can replace only components');
-   }
+/**
+ * Функция для замены одного компонента на другой
+ * @param {AbstractView} newComponent Компонент, который нужно показать
+ * @param {AbstractView} oldComponent Компонент, который нужно скрыть
+ */
+function replace(newComponent, oldComponent) {
+  if (!(newComponent instanceof AbstractView && oldComponent instanceof AbstractView)) {
+    throw new Error('Can replace only components');
+  }
 
-   const newElement = newComponent.element;
-   const oldElement = oldComponent.element;
+  const newElement = newComponent.element;
+  const oldElement = oldComponent.element;
 
-   const parent = oldElement.parentElement;
+  const parent = oldElement.parentElement;
 
-   if (parent === null) {
-     throw new Error('Parent element doesn\'t exist');
-   }
+  if (parent === null) {
+    throw new Error('Parent element doesn\'t exist');
+  }
 
-   parent.replaceChild(newElement, oldElement);
- }
+  parent.replaceChild(newElement, oldElement);
+}
 
- /**
-  * Функция для удаления компонента
-  * @param {AbstractView} component Компонент, который нужно удалить
-  */
- function remove(component) {
-   if (component === null) {
-     return;
-   }
+/**
+ * Функция для удаления компонента
+ * @param {AbstractView} componentOrComponents Компонент или компоненты, который нужно удалить
+ */
+function remove(componentOrComponents) {
+  if (Array.isArray(componentOrComponents)) {
+    componentOrComponents.forEach(remove);
+    return;
+  }
 
-   if (!(component instanceof AbstractView)) {
-     throw new Error('Can remove only components');
-   }
+  if (componentOrComponents === null) {
+    return;
+  }
 
-   component.element.remove();
-   component.removeElement();
- }
+  if (!(componentOrComponents instanceof AbstractView)) {
+    throw new Error('Can remove only components');
+  }
 
- export {RenderPosition, createElement, render, replace, remove};
+  componentOrComponents.element.remove();
+  componentOrComponents.removeElement();
+}
+
+export { RenderPosition, createElement, render, replace, remove };
