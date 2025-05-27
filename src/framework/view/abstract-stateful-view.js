21375f105f1fe695a27a5b47944389ec9897310b
Легков Никита <nik.legkov@mail.ru>
2025-05-27 09:56:39 +0500

    Правки перед защитой 2

--- src/framework/view/abstract-stateful-view.js
+++ src/framework/view/abstract-stateful-view.js
@@ -1,52 +1,52 @@
 import AbstractView from './abstract-view.js';
 
- /**
-  * Абстрактный класс представления с состоянием
-  */
- export default class AbstractStatefulView extends AbstractView {
-   /** @type {Object} Объект состояния */
-   _state = {};
+/**
+ * Абстрактный класс представления с состоянием
+ */
+export default class AbstractStatefulView extends AbstractView {
+  /** @type {Object} Объект состояния */
+  _state = {};
 
-   /**
-    * Метод для обновления состояния и перерисовки элемента
-    * @param {Object} update Объект с обновлённой частью состояния
-    */
-   updateElement(update) {
-     if (!update) {
-       return;
-     }
+  /**
+   * Метод для обновления состояния и перерисовки элемента
+   * @param {Object} update Объект с обновлённой частью состояния
+   */
+  updateElement(update) {
+    if (!update) {
+      return;
+    }
 
-     this._setState(update);
+    this._setState(update);
 
-     this.#rerenderElement();
-   }
+    this.#rerenderElement();
+  }
 
-   /**
-    * Метод для восстановления обработчиков после перерисовки элемента
-    * @abstract
-    */
-   _restoreHandlers() {
-     throw new Error('Abstract method not implemented: restoreHandlers');
-   }
+  /**
+   * Метод для восстановления обработчиков после перерисовки элемента
+   * @abstract
+   */
+  _restoreHandlers() {
+    throw new Error('Abstract method not implemented: restoreHandlers');
+  }
 
-   /**
-    * Метод для обновления состояния
-    * @param {Object} update Объект с обновлённой частью состояния
-    */
-   _setState(update) {
-     this._state = structuredClone({...this._state, ...update});
-   }
+  /**
+   * Метод для обновления состояния
+   * @param {Object} update Объект с обновлённой частью состояния
+   */
+  _setState(update) {
+    this._state = structuredClone({...this._state, ...update});
+  }
 
-   /** Метод для перерисовки элемента */
-   #rerenderElement() {
-     const prevElement = this.element;
-     const parent = prevElement.parentElement;
-     this.removeElement();
+  /** Метод для перерисовки элемента */
+  #rerenderElement() {
+    const prevElement = this.element;
+    const parent = prevElement.parentElement;
+    this.removeElement();
 
-     const newElement = this.element;
+    const newElement = this.element;
 
-     parent.replaceChild(newElement, prevElement);
+    parent.replaceChild(newElement, prevElement);
 
-     this._restoreHandlers();
-   }
- }
+    this._restoreHandlers();
+  }
+}
